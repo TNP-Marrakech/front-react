4dabb20e691cb1f9993b8cb5aa87b4cb67904174
unknown <ayoub.rhiti.1999@gmail.com>
2021-06-14 15:51:14 +0100

    last update Ayoub rhiti

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,70 @@
+import "./sidebar.css"
+import {
+    RssFeed,
+    Chat,
+    
+    Group,
+    Bookmark,
+    HelpOutline,
+    WorkOutline,
+    Event,
+    School,
+    
+  } from "@material-ui/icons";
+  import GroupWorkIcon from '@material-ui/icons/GroupWork';
+  import {Users} from "../../dummyData"
+import CloseFriend from "../closeFriend/CloseFriend";
+export default function Sidebar() {
+    return (
+        <div className="sidebar">
+            <div className="sidebarWrapper">
+                <ul className="sidebarList">
+                <li className="sidebarListItem">
+            <RssFeed className="sidebarIcon" />
+            <span className="sidebarListItemText">Actualités</span>
+          </li>
+          <li className="sidebarListItem">
+            <Chat className="sidebarIcon" />
+            <span className="sidebarListItemText">Espace Discussion</span>
+          </li>
+          <li className="sidebarListItem">
+            <GroupWorkIcon className="sidebarIcon" />
+            <span className="sidebarListItemText"> Salon Révision</span>
+          </li>
+          <li className="sidebarListItem">
+            <Group className="sidebarIcon" />
+            <span className="sidebarListItemText">Groupes</span>
+          </li>
+          <li className="sidebarListItem">
+            <Bookmark className="sidebarIcon" />
+            <span className="sidebarListItemText">Signets</span>
+          </li>
+          <li className="sidebarListItem">
+            <HelpOutline className="sidebarIcon" />
+            <span className="sidebarListItemText">Questions</span>
+          </li>
+          <li className="sidebarListItem">
+            <WorkOutline className="sidebarIcon" />
+            <span className="sidebarListItemText">Offres d'emploi</span>
+          </li>
+          <li className="sidebarListItem">
+            <Event className="sidebarIcon" />
+            <span className="sidebarListItemText">Evénements</span>
+          </li>
+          <li className="sidebarListItem">
+            <School className="sidebarIcon" />
+            <span className="sidebarListItemText">Cours et Formations</span>
+          </li>
+        </ul>
+        <button className="sidebarButton">Afficher plus</button>
+        <hr className="sidebarHr" />
+        <ul className="sidebarFriendList">
+            {Users.map(u=>(
+              <CloseFriend key={u.id} user={u}/>
+            ))}
+            
+        </ul>
+            </div>
+        </div>
+    )
+}
---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,45 @@
+import { useContext, useRef } from "react";
+import "./login.css";
+import { loginCall } from "../../apiCalls";
+import { AuthContext } from "../../context/AuthContext";
+import { CircularProgress } from "@material-ui/core";
+export default function Login() {
+  //hook
+  const email = useRef();
+  const password = useRef();
+  const { user , isFetching,  dispatch } = useContext(AuthContext);
+
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    loginCall(
+      { email: email.current.value, password: password.current.value },
+      dispatch
+    );
+  };
+  console.log(user);
+  return (
+    <div className="login">
+        
+      <div className="loginWrapper">
+        <div className="loginLeft">
+          <h3 className="loginLogo">Netsos</h3>
+          <span className="loginDesc">
+            Ici pour partager l'information avec les autres gratuitement.
+          </span>
+        </div>
+        <div className="loginRight">
+          <form className="loginBox" onSubmit={handleClick}>
+            <input placeholder="Email" type="email" className="loginInput" ref={email} required/>
+            <input placeholder="Password" type="password" className="loginInput" ref={password}  required minLength="6"/>
+            <button className="loginButton" disabled={isFetching} >{isFetching ? <CircularProgress color="white" size="20px"/> : "Se Connecter"}</button>
+            <span className="loginForgot">Mot de passe oublié ?</span>
+            <button className="loginRegisterButton">
+            {isFetching ? <CircularProgress color="white" size="20px"/> : "Créer Votre Compte"}
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
